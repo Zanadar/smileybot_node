0126a832b5855fd69ec8902249450c6cdc1e33b0
Zander Mackie <zander@skilledup.com>
2015-10-10 17:18:07 -0400

    Adjust port for heroku

--- smileybot.js
+++ smileybot.js
@@ -1,12 +1,13 @@
 var express = require ("express");
 var bodyParser = require("body-parser");
 var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
   console.log("Listening to your shit since day one on port %s...", server.address().port);
 });
